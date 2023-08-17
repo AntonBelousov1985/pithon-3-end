4afc00c00687e4a71a0b857ac449a8bc63f2be1d
Anton1985 <basket.f.g@yandex.ru>
2023-08-17 18:28:38 +0300

    3

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,10 +1,71 @@
 from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
+author = {
+    "Имя": "Иван",
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+
+}
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
+   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
+   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
+   {"id": 7, "name": "Картофель фри" ,"quantity":0},
+   {"id": 8, "name": "Кепка" ,"quantity":124},
+]
+
+
 def home(request):
-    text='''<h1>"Изучаем django"</h1>
-            <strong>Автор</strong>: <i>Белоусов А.И.</i>
-            '''
-    return HttpResponse(text)
+ #   text='''<h1>"Изучаем django"</h1>
+ #           <strong>Автор</strong>: <i>Белоусов А.И.</i>
+ #           '''
+#    return HttpResponse(text)
+    context = {
+        "name": "Белоусов Антон Иосифович",
+        "email": "my_mail@mail.ru"
+        }
+    return render(request, 'index.html', context)
+
+def about(request):
+    result = f"""
+    Имя: <b>{author['Имя']}</b><br>
+    Отчество: <b>{author['Отчество']}</b><br>
+    Фамилия: <b>{author['Фамилия']}</b><br>
+    телефон: <b>{author['телефон']}</b><br>
+    email: <b>{author['email']}</b><br>
+    """
+    return HttpResponse(result)
+
+
+def get_item(request, id):
+    """ По указанному id возвращает имя и количество"""
+    for item in items:
+        if item["id"] == id:
+            result = f""" 
+            <h2>Имя: {item["name"]}</h2>
+            <p> Количество: {item["quantity"]} </p>
+            <p><a href="/items">Назад к списку товаров</a></p>
+            """
+            return HttpResponse(result)
+    return HttpResponseNotFound(f'Item with id = {id} not found.')
+
+def items_list(request):
+ #   result='<h2>Список товаров</h2><ol>'
+ #   for item in items:
+ #       result+=f'''<li><a href="/item/{item["id"]}">{item["name"]}</li>'''
+ #   result+='<ol>'
+ #   return HttpResponse(result)
+    context={
+        "items": items
+    }
+    return render(request, "items-list.html",context)
+
+
+
