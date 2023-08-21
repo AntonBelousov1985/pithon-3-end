388c3b687090e7742eab795fd8d255f87e3efd6d
Anton1985 <basket.f.g@yandex.ru>
2023-08-21 14:00:16 +0300

    8

--- FirstDjango/settings.py
+++ FirstDjango/settings.py
@@ -115,9 +115,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+# english
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# russian
+# https://django.fun/ru/docs/django/4.1/howto/static-files/
+
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / "static",
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ MainApp_item.sql
@@ -0,0 +1,5 @@
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count") VALUES (1,'Кроссовки','abibas',10);
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count") VALUES (2,'Кепка','nike',124);
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count") VALUES (3,'Картофель фри','точка',0);
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count") VALUES (4,'Газировка','Coca-cola',12);
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count") VALUES (5,'Перчатки','lamoda',8);
