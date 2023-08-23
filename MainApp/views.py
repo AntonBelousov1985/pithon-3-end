4e9b1605a97678e76d4e608359303bbb271207f8
Anton1985 <basket.f.g@yandex.ru>
2023-08-23 14:52:28 +0300

    1.3

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from MainApp.models import Snippet
 
 
 def index_page(request):
@@ -14,5 +15,11 @@ def add_snippet_page(request):
 
 def snippets_page(request):
     snippets=Snippet.objects.all()
-    context = {'pagename': 'Просмотр сниппетов'}
+    context = {
+        'pagename': 'Просмотр сниппетов',
+        'snippets': snippets
+        }
     return render(request, 'pages/view_snippets.html', context)
+
+def snippet_detail(request):
+    pass
